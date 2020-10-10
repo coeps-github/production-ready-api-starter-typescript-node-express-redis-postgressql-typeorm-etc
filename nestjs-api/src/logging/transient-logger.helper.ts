590e614e6b4efad20fb16fb3a1e913f97e5ca797
Raphael Felber <coeps@coeps.com>
2020-10-10 04:38:33 +0200

    Fixed: object type

--- nestjs-api/src/logging/transient-logger.helper.ts
+++ nestjs-api/src/logging/transient-logger.helper.ts
@@ -1,4 +1,4 @@
-export function removePropertiesFromObject(properties: string[], obj: object): object {
+export function removePropertiesFromObject(properties: string[], obj: Record<string, unknown>): Record<string, unknown> {
   return Object.keys(obj).reduce((result, key) => {
     if (properties.includes(key)) {
       return result;
